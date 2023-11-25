2bb632b562a2fe01371d6aac5fd408f9edfdd3b2
RobertoGonzalesMatos <roberto_gonzales_matos@brown.edu>
2023-11-24 21:01:39 -0500

    Feat: hot fixes

--- frontend/repl/src/components/animations/flipcardRouts.tsx
+++ frontend/repl/src/components/animations/flipcardRouts.tsx
@@ -1,20 +1,29 @@
 import { ReactNode } from "react";
 import { motion } from "framer-motion";
-interface FlipContainerProps {
+interface FlipCardProps {
   children: ReactNode;
 }
-const FlipContainer: React.FC<FlipContainerProps> = ({ children }) => {
+const FlipCard: React.FC<FlipCardProps> = ({ children }) => {
   return (
     <motion.div
       style={{
         perspective: "1000px",
-        width: "100%",
-        height: "100%",
       }}
     >
-      {children}
+      <motion.div
+        initial={{ rotateY: -180 }}
+        animate={{ rotateY: 0 }}
+        exit={{ rotateY: 180 }}
+        transition={{ duration: 1 }}
+        style={{
+          transformStyle: "preserve-3d",
+          transformOrigin: "center",
+        }}
+      >
+        {children}
+      </motion.div>
     </motion.div>
   );
 };
 
-export default FlipContainer;
+export default FlipCard;
